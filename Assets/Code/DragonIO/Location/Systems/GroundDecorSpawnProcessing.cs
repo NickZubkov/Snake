57baea8ad67a389b0511e92ce6f09d6fdcfa30c1
Nick <zubk0vna31@gmail.com>
2022-06-02 10:43:40 +0500

    spawn decor on load

--- Assets/Code/DragonIO/Location/Systems/GroundDecorSpawnProcessing.cs
+++ Assets/Code/DragonIO/Location/Systems/GroundDecorSpawnProcessing.cs
@@ -15,18 +15,23 @@ namespace Modules.DragonIO.Location.Systems
             {
                 foreach (var levelData in _levelData)
                 {
-                    ref var levelRunTimeData = ref _levelData.Get1(levelData);
-                    ref var currentLevelConfigs = ref _levelData.Get2(levelData);
+                    ref var levelConfig = ref _levelData.Get2(levelData).GroundConfig;
+                    for (int i = 0; i < levelConfig.GroundDecorCount; i++)
+                    {
+                        ref var levelRunTimeData = ref _levelData.Get1(levelData);
+                        ref var currentLevelConfigs = ref _levelData.Get2(levelData);
 
-                    var randomPoint = Random.insideUnitCircle * levelRunTimeData.GroundDecorMaxSpawnRadius;
-                    var idx = Random.Range(0, currentLevelConfigs.GroundConfig.GroundDecorPrefabs.Count);
-                    var prefab = currentLevelConfigs.GroundConfig.GroundDecorPrefabs[idx];
-                    var position = new Vector3(randomPoint.x, 0, randomPoint.y);
-                    var scale = Random.Range(0.1f, 1f);
-                    var angle = new Vector3(0, Random.Range(0, 360f), 0);
-                    var groundDecor = Object.Instantiate(prefab, position, Quaternion.Euler(angle));
-                    groundDecor.transform.GetChild(0).localScale *= scale;
-                    groundDecor.Spawn(_world.NewEntity(), _world);
+                        var randomPoint = Random.insideUnitCircle * levelRunTimeData.GroundDecorMaxSpawnRadius;
+                        var idx = Random.Range(0, currentLevelConfigs.GroundConfig.GroundDecorPrefabs.Count);
+                        var prefab = currentLevelConfigs.GroundConfig.GroundDecorPrefabs[idx];
+                        var position = new Vector3(randomPoint.x, 0, randomPoint.y);
+                        var scale = Random.Range(0.1f, 1f);
+                        var angle = new Vector3(0, Random.Range(0, 360f), 0);
+                        var groundDecor = Object.Instantiate(prefab, position, Quaternion.Euler(angle));
+                        groundDecor.transform.GetChild(0).localScale *= scale;
+                        groundDecor.Spawn(_world.NewEntity(), _world);
+                        i++;
+                    }
                 }
             }
         }
